bf44284a71174f7d319056350af42b3dad4fdbbf
kfbyhyq <ymyq1525@163.com>
2024-08-19 20:53:03 +0800

    test

--- update.js
+++ update.js
@@ -1,111 +1,182 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#update').addEventListener('click', function () {
-        const button = document.getElementById('update');
-        button.style.backgroundColor = '#ff9f18';   // 对应按钮变为橙色，表示运行中
-        chrome.tabs.create({ url: 'https://minesweeper.online/cn/marketplace', active: false }, function (tab1) {
-            const ti1 = tab1.id;
-            recur(ti1, 0);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'eventQuest') {
+        let eqInfo = request.eqInfo;
+        console.log('活动任务信息:', eqInfo);   // 在控制台打出结果
+        chrome.storage.local.set({ eqInfo: eqInfo });     // 保存数据
+        document.getElementById('updateEq').style.backgroundColor = '#4caf50';   // 将对应按钮变为绿色，表示提取成功
+        document.getElementById('flag2').textContent = 1;   // 设置成功标记
+        const output = eqInfo.map(item => item + '<br>').join('');
+        document.getElementById('eqInfo').innerHTML = output;
+    }
+});
 
-            function recur(tabId, i) {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#updateEq').addEventListener('click', function () {
+        const button = document.getElementById('updateEq');
+        button.style.backgroundColor = '#ff9f18';   // 对应按钮变为橙色，表示运行中
+        chrome.tabs.create({ url: 'https://minesweeper.online/cn/event-quests', active: false }, function (tab1) {
+            const ti2 = tab1.id;
+            recurEq(ti2, 0);
+
+            function recurEq(tabId, i) {
                 var maxI = 100;
                 var t0 = 200;
                 setTimeout(() => {
-                    extract(tabId);
-                    const flag = document.getElementById('flag1').textContent;
+                    extractEq(tabId);
+                    const flag = document.getElementById('flag2').textContent;
                     if (flag == 1 || i > maxI) {
                         chrome.tabs.remove(tabId, function() {});
                     } else {
-                        recur(tabId, i + 1);
+                        recurEq(tabId, i + 1);
                     }
                 }, i * t0);
             }
-
-            function extract(tabId) {
+            function extractEq(tabId) {
                 chrome.scripting.executeScript({
                     target: { tabId },
                     function: function () {
-                        var priceMap = [
-                            // ['Topaz', 'Ruby', 'Sapphire', 'Amethyst', 'Onyx', 'Aquamarine', 'Emerald', 'Garnet', 'Jade', 'Diamond'],
-                            ['黄玉', '红宝石', '蓝宝石', '紫水晶', '缟玛瑙', '海蓝宝石', '祖母绿', '石榴石', '碧玉', '钻石'],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            // ['Gold coins', 'Copper coins', 'Silver coins', 'Nickel coins', 'Steel coins', 'Iron coins', 'Palladium coins', 'Titanium coins', 'Zinc coins', 'Platinum coins'],
-                            ['金竞技场币', '铜竞技场币', '银竞技场币', '镍竞技场币', '钢竞技场币', '铁竞技场币', '钯竞技场币', '钛竞技场币', '锌竞技场币', '铂竞技场币'],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            // ['Rare parts', 'Unique parts', 'Legendary parts', 'Perfect parts'],
-                            ['稀有碎片', '史诗碎片', '传说碎片', '完美碎片'],
-                            [0, 0, 0, 0]
-                        ];
+                        var index = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                        var name = ['竞技场门票', '竞技场', '效率', '连胜', '动态胜率', '盲扫', '无猜', '自定义', '金币', '宝石', '速度', '初级局数', '中级局数', '高级局数'];
+                        var keyword = ['门票', '竞技场', '效率', '连胜', '百局', '盲扫', 'NG', '自定义', '金币', '获得', '用时', '初级', '中级', '高级'];
+                        var restrict = [[15, 25], '', '', '', ['20E', '30E'], [4, 30], '', '', '', '', [4, 6], [4, 10], '', ''];
+                        var level = [2, 4, 1, 3, 5, 2, 4, 1, 3, 6];
+                        var next = -1;
+                        var levelRange = [[4, 7], [8, 11], [12, 15], [16, 20], [20, 30], ['20E', '30E']];
+                        var eqInfo = ['下一任务等级', '可用任务列表：', '', '', '', '', '', '距离', '', '近10个任务：', '', '', '', '', '', '', '', '', '', ''];
+                        var secShift = 3;
+                        var secCycle = 19;
                         try {
-                            var Topaz = document.querySelector("#stat_table_body > tr:nth-child(1) > td:nth-child(3)");
-                            priceMap[1][0] = Topaz.textContent;
-                            var Ruby = document.querySelector("#stat_table_body > tr:nth-child(2) > td:nth-child(3)");
-                            priceMap[1][1] = Ruby.textContent;
-                            var Sapphire = document.querySelector("#stat_table_body > tr:nth-child(3) > td:nth-child(3)");
-                            priceMap[1][2] = Sapphire.textContent;
-                            var Amethyst = document.querySelector("#stat_table_body > tr:nth-child(4) > td:nth-child(3)");
-                            priceMap[1][3] = Amethyst.textContent;
-                            var Onyx = document.querySelector("#stat_table_body > tr:nth-child(5) > td:nth-child(3)");
-                            priceMap[1][4] = Onyx.textContent;
-                            var Aquamarine = document.querySelector("#stat_table_body > tr:nth-child(6) > td:nth-child(3)");
-                            priceMap[1][5] = Aquamarine.textContent;
-                            var Emerald = document.querySelector("#stat_table_body > tr:nth-child(7) > td:nth-child(3)");
-                            priceMap[1][6] = Emerald.textContent;
-                            var Garnet = document.querySelector("#stat_table_body > tr:nth-child(8) > td:nth-child(3)");
-                            priceMap[1][7] = Garnet.textContent;
-                            var Jade = document.querySelector("#stat_table_body > tr:nth-child(9) > td:nth-child(3)");
-                            priceMap[1][8] = Jade.textContent;
-                            var Diamond = document.querySelector("#stat_table_body > tr:nth-child(10) > td:nth-child(3)");
-                            priceMap[1][9] = Diamond.textContent;
-
-                            var Gold = document.querySelector("#stat_table_body > tr:nth-child(11) > td:nth-child(3)");
-                            priceMap[3][0] = Gold.textContent;
-                            var Copper = document.querySelector("#stat_table_body > tr:nth-child(12) > td:nth-child(3)");
-                            priceMap[3][1] = Copper.textContent;
-                            var Silver = document.querySelector("#stat_table_body > tr:nth-child(13) > td:nth-child(3)");
-                            priceMap[3][2] = Silver.textContent;
-                            var Nickel = document.querySelector("#stat_table_body > tr:nth-child(14) > td:nth-child(3)");
-                            priceMap[3][3] = Nickel.textContent;
-                            var Steel = document.querySelector("#stat_table_body > tr:nth-child(15) > td:nth-child(3)");
-                            priceMap[3][4] = Steel.textContent;
-                            var Iron = document.querySelector("#stat_table_body > tr:nth-child(16) > td:nth-child(3)");
-                            priceMap[3][5] = Iron.textContent;
-                            var Palladium = document.querySelector("#stat_table_body > tr:nth-child(17) > td:nth-child(3)");
-                            priceMap[3][6] = Palladium.textContent;
-                            var Titanium = document.querySelector("#stat_table_body > tr:nth-child(18) > td:nth-child(3)");
-                            priceMap[3][7] = Titanium.textContent;
-                            var Zinc = document.querySelector("#stat_table_body > tr:nth-child(19) > td:nth-child(3)");
-                            priceMap[3][8] = Zinc.textContent;
-                            var Platinum = document.querySelector("#stat_table_body > tr:nth-child(20) > td:nth-child(3)");
-                            priceMap[3][9] = Platinum.textContent;
-
-                            var Rare = document.querySelector("#stat_table_body > tr:nth-last-child(4) > td:nth-child(3)");
-                            priceMap[5][0] = Rare.textContent.replace(/ /g, "");
-                            var Unique = document.querySelector("#stat_table_body > tr:nth-last-child(3) > td:nth-child(3)");
-                            priceMap[5][1] = Unique.textContent.replace(/ /g, "");
-                            var Legendary = document.querySelector("#stat_table_body > tr:nth-last-child(2) > td:nth-child(3)");
-                            priceMap[5][2] = Legendary.textContent.replace(/ /g, "");
-                            var Perfect = document.querySelector("#stat_table_body > tr:nth-last-child(1) > td:nth-child(3)");
-                            priceMap[5][3] = Perfect.textContent.replace(/ /g, "");
-
-                            console.log(priceMap);
-                            chrome.runtime.sendMessage({ action: 'sendGemsPrice', gemsPrice: priceMap });
-                        } catch (e) {
-                            console.error('错误页面', e);
+                            for (let i = 0; i < 10; i++) {
+                                let questLevel = document.querySelector(`#QuestsBlock > table:nth-child(9) > tbody > tr:nth-child(${i+1}) > td:nth-child(1)`);
+                                let questContent = document.querySelector(`#QuestsBlock > table:nth-child(9) > tbody > tr:nth-child(${i+1}) > td:nth-child(2)`);
+                                if (questContent && questLevel) {
+                                    let ql = questLevel.textContent;
+                                    let qc = questContent.textContent;
+                                    eqInfo[10+i] = ql + '  ' + qc;
+                                    // console.log(eqInfo[10+i]);
+                                    if (ql.includes('E')) {
+                                        next = i;
+                                    }
+                                    for (let j = 0; j < 14; j++) {
+                                        if (qc.includes(keyword[j])) {
+                                            index[j]++;
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    break;
+                                }
+                            }
+                            let firstId = document.querySelector("#QuestsBlock > table:nth-child(9) > tbody > tr:nth-child(1)").id.match(/\d+$/)[0];
+                            var secret = [];
+                            var toE = [];
+                            var nextLevel = [];
+                            var nextRange = [];
+                            if (secShift) { secret = secCycle - (+firstId + secShift) % secCycle; } else { secret = '未知'; }
+                            if (next < 0) {
+                                let firstLevel = document.querySelector('#QuestsBlock > table:nth-child(9) > tbody > tr:nth-child(1) > td:nth-child(1)').textContent.match(/\d+$/)[0];
+                                console.log(firstLevel);
+                                if (firstLevel <= levelRange[0][1]) { 
+                                    next = 3;  // 3或8，level=3
+                                    toE = '1或6';
+                                } else if (firstLevel <= levelRange[1][1]) {
+                                    next = 1; // 1或6，level=4
+                                    toE = '3或8';
+                                } else if (firstLevel <= levelRange[2][1]) {
+                                    next = -1; // 4或9，level=5或6E
+                                    toE = '0或5';
+                                } else if (firstLevel < levelRange[3][1]) {
+                                    next = 2; // 2或7，level=1
+                                    toE = '2或7';
+                                } else if (firstLevel = levelRange[3][1]) {
+                                    next = -1; // 2/7/0/5，level=1/2
+                                    toE = '未知';
+                                } else {
+                                    next = 5; //level=2
+                                    toE = 4;
+                                }
+                            } else { toE = 9 - next; }
+    
+                            if (next < 0) {
+                                nextRange = '未知';
+                            } else {
+                                nextLevel = level[next] - 1;
+                                nextRange = 'L' + levelRange[nextLevel][0] + '-' + levelRange[nextLevel][1];
+                            }
+                            eqInfo[7] = '距离机密：' + secret + '，距离E：' + toE;
+                            if (secret == 0) {
+                                eqInfo[0] = '下一任务等级：' + nextRange + ' 【机密】';
+                            } else {
+                                eqInfo[0] = '下一任务等级：' + nextRange;
+                            }
+    
+                            var row = 2;
+                            if (next < 0) {
+                                for (let k = 0; k < 14; k++) {
+                                    if (index[k] == 0) {
+                                        eqInfo[row] = ' √ ' + name[k];
+                                        row++;
+                                    }
+                                }
+                            } else {
+                                for (let k = 0; k < 14; k++) {
+                                    if (index[k] == 0) {
+                                        if (restrict[k]) {
+                                            if (restrict[k][0] == '20E') {
+                                                if (next == 9) {
+                                                    eqInfo[row] = ' √ ' + name[k];
+                                                    row++;
+                                                } else {
+                                                    eqInfo[row] = '（不可用）' + name[k] + '（限制为L' + restrict[k][0] + '-' + restrict[k][1] + '）';
+                                                    row++;
+                                                }
+                                            } else if (next == 9) {
+                                                eqInfo[row] = '（不可用）' + name[k] + '（限制为L' + restrict[k][0] + '-' + restrict[k][1] + '）';
+                                                row++;
+                                            } else {
+                                                if (restrict[k][0] <= levelRange[nextLevel][0]) {
+                                                    if (restrict[k][1] >= levelRange[nextLevel][1]) {
+                                                        eqInfo[row] = ' √ ' + name[k];
+                                                        row++;
+                                                    } else if (restrict[k][1] >= levelRange[nextLevel][0]) {
+                                                        eqInfo[row] = ' √ ' + name[k] + '（限制L' + levelRange[nextLevel][0] + '-' + restrict[k][1] + '）';
+                                                        row++;
+                                                    } else {
+                                                        eqInfo[row] = '（不可用）' + name[k] + '（限制为L' + restrict[k][0] + '-' + restrict[k][1] + '）';
+                                                        row++;
+                                                    }
+                                                } else if (restrict[k][0] <= levelRange[nextLevel][1]) {
+                                                    if (restrict[k][1] >= levelRange[nextLevel][1]) {
+                                                        eqInfo[row] = ' √ ' + name[k] + '（限制L' + restrict[k][0] + '-' + levelRange[nextLevel][1] + '）';
+                                                        row++;
+                                                    } else {
+                                                        eqInfo[row] = ' √ ' + name[k] + '（限制为L' + restrict[k][0] + '-' + restrict[k][1] + '）';
+                                                        row++;
+                                                    }
+                                                } else {
+                                                    eqInfo[row] = '（不可用）' + name[k] + '（限制为L' + restrict[k][0] + '-' + restrict[k][1] + '）';
+                                                    row++;
+                                                }
+                                            }
+                                        } else {
+                                            eqInfo[row] = ' √ ' + name[k];
+                                            row++;
+                                        }
+                                        if (row > 6) {
+                                            eqInfo.splice(row, 0, '');
+                                        }
+                                    }
+                                }
+                            }
+                            console.log(eqInfo);
+    
+                            chrome.runtime.sendMessage({ action: 'eventQuest', eqInfo: eqInfo });
+                        } catch (error) {
+                            console.log(error);
                         }
-
                     }
                 });
             }
         });
     });
-});
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'sendGemsPrice') {
-        let gemsPrice = request.gemsPrice;
-        console.log('收到价格更新:', gemsPrice);   // 在控制台打出结果
-        chrome.storage.local.set({ gemsPrice: gemsPrice });     // 保存数据
-        document.getElementById('update').style.backgroundColor = '#4caf50';   // 将对应按钮变为绿色，表示提取成功
-        document.getElementById('flag1').textContent = 1;   // 设置成功标记
-    }
 });
